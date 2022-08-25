d9bd387a5474dc10252085050d7e325dbaf5b65a
btxuyenhcmus <btxuyenhcmus@gmail.com>
2022-08-25 14:50:46 +0700

    [ADD] cart: added template

--- xshopping/views.py
+++ xshopping/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from store.models import Product
+from category.models import Category
 
 
 def home(request):
     products = Product.objects.all().filter(is_available=True)
+    links = Category.objects.all()
     context = {
         'products': products,
+        'links': links
     }
     return render(request, 'home.html', context=context)
